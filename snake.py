5515dbc62c5e2a4dbd4c1cc0fc851a66d1ce6ae7
Omee63 <shariaromee63@gmail.com>
2023-01-19 17:39:05 +0600

    Adding Snake Game to the repository

---
+++ snake.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_xy = self.segments[seg_num - 1].position()
+            self.segments[seg_num].goto(new_xy)
+        self.segments[0].forward(20)
